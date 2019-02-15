75b9b21dd4d3ebbe1e00b65b434b3c1f4ea446b5
tariq-rasheed <tariq.rasheed@peeredge.io>
2019-02-15 18:43:09 +0500

    reset theme

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -61,9 +61,9 @@ module.exports = options => ({
             options: {
               javascriptEnabled: true,
               modifyVars: {
-                'primary-color': '#1DA57A',
-                'link-color': '#1DA57A',
-                'border-radius-base': '2px',
+                // 'primary-color': '#1DA57A',
+                // 'link-color': '#1DA57A',
+                // 'border-radius-base': '2px',
               },
             },
           },
